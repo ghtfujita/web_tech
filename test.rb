c0fd9af9b25e1de1a15d28ed7f2a0fd56614fef4
takeshi fujita <mail.takeshi.fujita@gmail.com>
2020-11-17 11:32:46 +0900

    assignment submitted

--- test.rb
+++ test.rb
@@ -1,11 +1,16 @@
 require 'webrick'
-server = WEBrick::HTTPServer.new ({
-  :DOcumentRoot => '.',
-  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
-  :Port => '3000',
+server = WEBrick::HTTPServer.new({
+    :DocumentRoot => '.',
+    :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+    :Port => '3000'
 })
 ['INT', 'TERM'].each {|signal|
 Signal.trap(signal){ server.shutdown }
 }
 server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'index.html.erb')
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/goya_nonHome.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_nonHome.rb')
+server.mount('/goya_loQ.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_loQ.rb')
 server.start
